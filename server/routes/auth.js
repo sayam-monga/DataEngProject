548c7d75b45b64ea2205dc9ee1f9a069f13b9e81
Angad Suryavanshi <135613657+angad2212@users.noreply.github.com>
2025-04-20 22:52:58 +0530

    commit

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const auth = require('../middleware/auth');
+
 
 const router = express.Router();
 
