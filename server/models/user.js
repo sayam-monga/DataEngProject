d68fc631d8149315d27ba4e2146d0cc1961f8423
Angad Suryavanshi <135613657+angad2212@users.noreply.github.com>
2025-04-20 21:54:42 +0530

    main branch check

--- server/models/user.js
+++ server/models/user.js
@@ -16,4 +16,4 @@ const userSchema = new mongoose.Schema({
   reviews: [userReviewSchema]
 });
 
-module.exports = mongoose.model('User', userSchema); //new
+module.exports = mongoose.model('User', userSchema); 
