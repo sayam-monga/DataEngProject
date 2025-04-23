744e223c7229e807609d86c25f1295e47a1d9edc
Angad Suryavanshi <135613657+angad2212@users.noreply.github.com>
2025-04-23 10:00:09 +0530

    new movie changes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
-const movieRoutes = require('./routes/movies'); //movoes
+const movieRoutes = require('./routes/movies'); //movies
 
 require('dotenv').config();
 
